49668ecd6d9933963151982f51b180f4854d75d4
cynthia <cynthiakagz@gmail.com>
2023-09-06 21:31:02 +0300

    pet.js

--- src/components/Pet/Pet.js
+++ src/components/Pet/Pet.js
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import './Pet.css'
+// import React, { useState, useEffect } from 'react';
+// import './Pet.css'
 
-function Pet () {
-  const [pets, setPets] = useState([]);
+// function Pet () {
+//   const [pets, setPets] = useState([]);
 
-  useEffect(() => {
-    fetch('')
-      .then((response) => response.json())
-      .then((data) => setPets(data))
-      .catch((error) => console.error('Error fetching pets:', error));
-  }, []);
+//   useEffect(() => {
+//     fetch('')
+//       .then((response) => response.json())
+//       .then((data) => setPets(data))
+//       .catch((error) => console.error('Error fetching pets:', error));
+//   }, []);
 
-  return (
-    <div className="pet-container">
-      <h2>Pets</h2>
-      <div className="pet-list">
-        {pets.map((pet) => (
-          <div key={pet.id} className="pet-item">
-            <img src={pet.image} alt={pet.name} />
-            <h3>{pet.name}</h3>
-            <h3>{pet.age}</h3>
-            <h3>{pet.about}</h3>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="pet-container">
+//       <h2>Pets</h2>
+//       <div className="pet-list">
+//         {pets.map((pet) => (
+//           <div key={pet.id} className="pet-item">
+//             <img src={pet.image} alt={pet.name} />
+//             <h3>{pet.name}</h3>
+//             <h3>{pet.age}</h3>
+//             <h3>{pet.about}</h3>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Pet;
+// export default Pet;
 
 import React, { useState, useEffect } from 'react';
 import { Card, Image } from 'semantic-ui-react';
