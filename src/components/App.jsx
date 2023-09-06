832b42215e8e3fac2781787dd803ed1ea0e1d72f
Sharon Awuor <sawuor395@gmail.com>
2023-09-06 21:47:36 +0300

    const for loggedInUser

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,3 +3,6 @@ import { useLogin } from "./UserContext";
 
 const PETS = [{ name: "Pet 1" }, { name: "Pet 2" }, { name: "Pet 3" }];
 export default function App() {
+    const { loggedInUser } = useLogin();
+  return (
+    <></>
