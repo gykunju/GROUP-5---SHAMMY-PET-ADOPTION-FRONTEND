07b9a6face641bb354d05a6f972131783e3d1571
cynthia <cynthiakagz@gmail.com>
2023-09-06 21:43:40 +0300

    updated pet.js

--- src/components/Pet/Pet.js
+++ src/components/Pet/Pet.js
@@ -32,3 +32,42 @@
 // }
 
 // export default Pet;
+
+
+
+
+import React, { useState, useEffect } from 'react';
+import { Card, Image } from 'semantic-ui-react';
+import './Pet.css';
+
+function Pet() {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from your backend API
+    fetch('YOUR_BACKEND_API_ENDPOINT_HERE') // Replace with your backend API endpoint
+      .then((response) => response.json())
+      .then((data) => setPets(data))
+      .catch((error) => console.error('Error fetching pets:', error));
+  }, []);
+
+  return (
+    <div className="pet-container">
+      <h1 className="pet-title">Pet List</h1>
+      <Card.Group>
+        {pets.map((pet) => (
+          <Card key={pet.id} className="pet-card">
+            <Image src={pet.image_url} alt={pet.name} className="pet-image" />
+            <Card.Content>
+              <Card.Header>{pet.name}</Card.Header>
+              <Card.Meta>Age: {pet.age}</Card.Meta>
+              <Card.Description className="pet-details">About: {pet.about}</Card.Description>
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
+    </div>
+  );
+}
+
+export default Pet;
