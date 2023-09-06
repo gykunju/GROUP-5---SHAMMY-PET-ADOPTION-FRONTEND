fd7f15a819b24fcfa9fdb1cead07a9901cb0410f
Sharon Awuor <sawuor395@gmail.com>
2023-09-06 23:47:34 +0300

    json response

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,7 +8,7 @@ export default function Login() {
     const [password, setPassword] = useState("");
     const { login } = useLogin();
     const navigate = useNavigate();
-    
+
     function handleLogin(e) {
         e.preventDefault();
         fetch("http://127.0.0.1:3000/login", {
@@ -16,3 +16,9 @@ export default function Login() {
           body: JSON.stringify({ email, password }),
           headers: { "Content-Type": "application/json" }
         })
+        .then((res) => res.json())
+      .then((data) => {
+        login(data);
+        navigate("/");
+      });
+  }
