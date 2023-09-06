056d40aede53a7af92d99252c41aaf3623ca8513
Sharon Awuor <sawuor395@gmail.com>
2023-09-06 21:49:18 +0300

    conditional rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,4 +5,6 @@ const PETS = [{ name: "Pet 1" }, { name: "Pet 2" }, { name: "Pet 3" }];
 export default function App() {
     const { loggedInUser } = useLogin();
   return (
-    <></>
+    <>
+    {loggedInUser.admin && <button>Create Pet</button>}
+    </>
