68361cca35b61359b6ab7b4ed7e77b39327d327b
Sharon Awuor <sawuor395@gmail.com>
2023-09-07 00:29:10 +0300

    usercontext

--- src/components/UserContext.js
+++ src/components/UserContext.js
@@ -4,3 +4,18 @@ const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
     const [loggedInUser, setLoggedInUser] = useState({});
+      
+        function login(data) {
+          setLoggedInUser(data);
+        }
+        return (
+          <UserContext.Provider value={{ loggedInUser, login }}>
+            {children}
+          </UserContext.Provider>
+        );
+      };
+      
+      export function useLogin() {
+        return useContext(UserContext);
+      }
+   
