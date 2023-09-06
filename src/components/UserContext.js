887a172c3f428e163b3c833fb04978b6c0c79635
Sharon Awuor <sawuor395@gmail.com>
2023-09-07 00:22:20 +0300

    export

--- src/components/UserContext.js
+++ src/components/UserContext.js
@@ -1,3 +1,6 @@
 import { createContext, useState, useContext } from "react";
 
 const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+    const [loggedInUser, setLoggedInUser] = useState({});
