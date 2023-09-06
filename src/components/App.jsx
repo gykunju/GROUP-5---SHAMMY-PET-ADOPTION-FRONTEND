59233eb003511199afcfa3a1dcdc80cc56b9b581
Sharon Awuor <sawuor395@gmail.com>
2023-09-06 21:55:11 +0300

    adding hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import "./styles.css";
 import { useLogin } from "./UserContext";
+import React, { useEffect, useState } from 'react';
 
 const PETS = [{ name: "Pet 1" }, { name: "Pet 2" }, { name: "Pet 3" }];
 export default function App() {
