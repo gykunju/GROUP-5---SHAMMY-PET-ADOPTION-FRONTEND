0a6bec5e28657ccbca6c5a2e17c34a9560bf1a47
Sharon Awuor <sawuor395@gmail.com>
2023-09-06 21:57:39 +0300

    useState for pets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,14 @@ import "./styles.css";
 import { useLogin } from "./UserContext";
 import React, { useEffect, useState } from 'react';
 
-const PETS = [{ name: "Pet 1" }, { name: "Pet 2" }, { name: "Pet 3" }];
+// const PETS = [{ name: "Pet 1" }, { name: "Pet 2" }, { name: "Pet 3" }];
 export default function App() {
+    const [pets, setPets] = useState([]);
     const { loggedInUser } = useLogin();
-  return (
+
     <>
     {loggedInUser.admin && <button>Create Pet</button>}
-    {PETS.map((pet) => (
+    {pets.map((pet) => (
         <div key={pet.name}>
           <p>{pet.name}</p>
           {loggedInUser.admin && (
