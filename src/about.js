5aa4b67c310d24d2e5a5a6d07c690175049f60da
samuel <samuelkelvinmwangi@gmail.com>
2023-09-07 14:07:33 +0300

    adding css

--- src/about.js
+++ src/about.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const About = () => {
   return (
+    <div className='about'>
     <div className="max-width">
     <h2 className="title">SHAMMY PET ADOPTION</h2>
     <div className="about-content">
@@ -13,6 +14,7 @@ const About = () => {
                     <p>Pet adoption is a compassionate and life-changing choice that not only enriches the lives of the animals involved but also brings immeasurable joy to their new human companions. At our website, we believe in the power of second chances, providing loving homes to pets in need. Each adoption story is a testament to the incredible bond that forms between humans and their furry friends. Whether you're seeking a loyal canine companion, a playful feline friend, or any other type of pet, adopting is a rewarding experience that opens your heart to unconditional love.</p>
                 </div>
             </div>
+        </div>
         </div>
   );
 };
