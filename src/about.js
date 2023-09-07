8f55aadb33acc6fb6736f2953cf24f684a4fbba4
samuel <samuelkelvinmwangi@gmail.com>
2023-09-07 12:03:59 +0300

    updating the about contetnt

--- src/about.js
+++ src/about.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p></p>
-    </div>
+    <div class="max-width">
+            <h2 class="title">SHAMMY PET ADOPTION</h2>
+            <div class="about-content">
+                <div class="column left container">
+                </div>
+                <div class="column right">
+                    <div class="text">We are Adoption <span>Website</span></div>
+                    <p>Pet adoption is a compassionate and life-changing choice that not only enriches the lives of the animals involved but also brings immeasurable joy to their new human companions. At our website, we believe in the power of second chances, providing loving homes to pets in need. Each adoption story is a testament to the incredible bond that forms between humans and their furry friends. Whether you're seeking a loyal canine companion, a playful feline friend, or any other type of pet, adopting is a rewarding experience that opens your heart to unconditional love.</p>
+                </div>
+            </div>
+        </div>
   );
 };
 
