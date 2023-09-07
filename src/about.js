ec768fbbb8721f746dda5429103ea0aef0cbc9c1
samuel <samuelkelvinmwangi@gmail.com>
2023-09-07 12:21:26 +0300

    adding picture and and assets folder

--- src/about.js
+++ src/about.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div class="max-width">
-            <h2 class="title">SHAMMY PET ADOPTION</h2>
-            <div class="about-content">
-                <div class="column left container">
-                </div>
-                <div class="column right">
-                    <div class="text">We are Adoption <span>Website</span></div>
+    <div className="max-width">
+    <h2 className="title">SHAMMY PET ADOPTION</h2>
+    <div className="about-content">
+      <div className="column left container">
+        <img src="/GROUP-5---SHAMMY-PET-ADOPTION-FRONTEND/src/assets/pet adoption.jpg" alt="about-img" className="about-img" />
+      </div>
+      <div className="column right">
+        <div className="text">We are Adoption <span>Website</span></div>
                     <p>Pet adoption is a compassionate and life-changing choice that not only enriches the lives of the animals involved but also brings immeasurable joy to their new human companions. At our website, we believe in the power of second chances, providing loving homes to pets in need. Each adoption story is a testament to the incredible bond that forms between humans and their furry friends. Whether you're seeking a loyal canine companion, a playful feline friend, or any other type of pet, adopting is a rewarding experience that opens your heart to unconditional love.</p>
                 </div>
             </div>
