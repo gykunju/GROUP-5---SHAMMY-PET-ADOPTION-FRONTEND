cf18c4d3036c8678381112cd29c70b3cb33ba9b7
gykunju <alvingikunju@gmail.com>
2023-09-07 08:56:19 +0300

    add form

---
+++ src/components/AdptionForm.js
@@ -0,0 +1,33 @@
+import React,{useState} from 'react'
+import { useParams } from 'react-router-dom'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom'
+
+function AdoptionForm() {
+  
+  const [location, setLocation] =useState(null)
+  const [number, setNumber] =useState(null)
+  const [reason, setReason] =useState(null)
+  const { id } = useParams()
+  const history = useHistory()
+
+
+  return (
+    <div className='adopt-div'>
+      <h2>Adopt THEM</h2>
+      <form onSubmit={handleSubmit} className='adopt-form'>
+        <label>Location :
+          <input placeholder='location' value={location} onChange={(e)=>setLocation(e.value)}/>
+        </label>
+        <label>Number :
+          <input placeholder='number' value={number} onChange={(e)=>setNumber(e.value)}/>
+        </label>
+        <label>Reason : 
+          <input placeholder='reason' value={reason} onChange={(e)=>setReason(e.value)}/>
+        </label>
+        <button type='submit'>ADOPT</button>
+      </form>
+    </div>
+  )
+}
+
+export default AdoptionForm
